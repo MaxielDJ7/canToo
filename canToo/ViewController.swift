df92e65220ff0baba3c33beed9d1245a497891d1
MaxielDJ7 <mid6>
2017-04-06 20:31:34 -0400

    CanToo
    CanToo is a motivational IOS app that generates a positive quote
    whenever the Tut the Toucan is tapped.

--- canToo/ViewController.swift
+++ canToo/ViewController.swift
@@ -9,17 +9,41 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // MARK: Properties
 
+    @IBOutlet weak var toucanTut: UIImageView!
+    @IBOutlet weak var canTooLabel: UILabel!
+    
+    let tapRecon = UITapGestureRecognizer()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        tapRecon.addTarget(self, action: #selector(ViewController.tappedView))
+        toucanTut.isUserInteractionEnabled = true;
+        
+        toucanTut.addGestureRecognizer(tapRecon)
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    //MARK: Actions
+    
+    func tappedView(){
+        let popOverVC = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "quotePopUp") as! PopUpViewController
+        self.addChildViewController(popOverVC)
+        popOverVC.view.frame = self.view.frame
+        self.view.addSubview(popOverVC.view)
+        popOverVC.didMove(toParentViewController: self)
+    }
+    
 }
+
 
